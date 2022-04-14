7f4dd95a93b6b2104dc4f4e21f034fb4fcac8ca8
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-14 02:32:04 -0600

    overhauled design and content. added menu (static for now), added Like buttons with alert on click.

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-/*const h2 = document.createElement("h2");
+const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2); */
+document.querySelector("body").appendChild(h2); 
 
+document.getElementById("myButton1").onclick = function()
+{
+    alert("Liked!");
+}
+document.getElementById("myButton2").onclick = function()
+{
+    alert("Liked!");
+}
+document.getElementById("myButton3").onclick = function()
+{
+    alert("Liked!");
+}
+
+/*const like = document.getElementsByClassName("button");
+like.addEventListener('click', function() {
+  alert('Liked!');
+});*/
+
+
+/*function clickAlert() {
+    alert('Liked!');
+  }
+const like = document.getElementsByTagName('button');
+for (const key in like) {
+like.addEventListener('click', clickAlert);
+}*/
